1866035a582f1cc585814767fd422d80fdbc6adb
astaviolet <134850469+astaviolet@users.noreply.github.com>
2023-09-27 00:18:44 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado');
+
+    socket.on('message', (msg) => {
+        io.emit('message', msg); // Envia a mensagem para todos os usuários
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
